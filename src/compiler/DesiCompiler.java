8be47bd601a76469960190e72474e9b0af13d191
virajtalaty <virajtalaty@gmail.com>
2019-04-21 16:25:22 -0700

    Adding Methods to Compiler

--- src/compiler/DesiCompiler.java
+++ src/compiler/DesiCompiler.java
@@ -6,13 +6,35 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 import desi.DesiGrammarBaseVisitor;
 
 import desi.DesiGrammarParser;
+import desi.DesiGrammarParser.BooleanexpContext;
+import desi.DesiGrammarParser.CommandContext;
 import desi.DesiGrammarParser.DeclarationContext;
 import desi.DesiGrammarParser.FactorContext;
 import desi.DesiGrammarParser.ProgramContext;
 import desi.DesiGrammarParser.TermContext;
+import desi.DesiGrammarParser.UnitContext;
 
 public class DesiCompiler extends DesiGrammarBaseVisitor{
 
+	
+
+	private class IntermediateCode {
+        private String IntermediateOutput = "";
+        private void addIntermediateOutput(String output) {
+            this.IntermediateOutput += output + "\n";
+        }
+    }
+	
+	private IntermediateCode intermediateCodeGenerator = new IntermediateCode();
+	
+	public String getOutput() {
+		return intermediateCodeGenerator.IntermediateOutput;
+	}
+	
+	
+	
+	
+	
 	@Override
 	public Object visitProgram(ProgramContext ctx) {
 		return visitChildren(ctx); 
@@ -55,5 +77,23 @@ public class DesiCompiler extends DesiGrammarBaseVisitor{
 		return super.visitFactor(ctx);
 	}
 	
+
+	@Override
+	public Object visitCommand(CommandContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitCommand(ctx);
+	}
+	
+	@Override
+	public Object visitUnit(UnitContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitUnit(ctx);
+	}
+	
+	@Override
+	public Object visitBooleanexp(BooleanexpContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitBooleanexp(ctx);
+	}
 	
 }
