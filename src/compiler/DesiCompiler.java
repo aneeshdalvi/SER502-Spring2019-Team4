e450a4d6759568b19e533bab46c82badce0c8d31
Janice Abraham <janicejabraham@gmail.com>
2019-04-21 16:21:40 -0700

    Adding methods in DesiCompiler

--- src/compiler/DesiCompiler.java
+++ src/compiler/DesiCompiler.java
@@ -3,6 +3,7 @@ package compiler;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 import desi.DesiGrammarBaseVisitor;
+import desi.DesiGrammarParser;
 import desi.DesiGrammarParser.ProgramContext;
 
 public class DesiCompiler extends DesiGrammarBaseVisitor{
@@ -10,6 +11,25 @@ public class DesiCompiler extends DesiGrammarBaseVisitor{
 	@Override
 	public Object visitProgram(ProgramContext ctx) {
 		return visitChildren(ctx); 
+	}
+	
+	@Override
+	public Object visitBlock(DesiGrammarParser.BlockContext ctx)
+	{
+		return null;
+	}
+	
+	
+	@Override
+	public Object visitExpression(DesiGrammarParser.ExpressionContext ctx)
+	{
+		return null;
+	}
+	
+	@Override
+	public Object visitExpression1(DesiGrammarParser.Expression1Context ctx)
+	{
+		return null;
 	}
 	
 }
