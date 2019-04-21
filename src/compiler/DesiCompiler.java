ab29692bb0bde217b3cc4c372424b4196c79c43d
aneeshdalvi <adalvi1@asu.edu>
2019-04-21 16:18:49 -0700

    Added necessary overidden methods to compiler file

--- src/compiler/DesiCompiler.java
+++ src/compiler/DesiCompiler.java
@@ -1,9 +1,13 @@
 package compiler;
 
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 import desi.DesiGrammarBaseVisitor;
+import desi.DesiGrammarParser.DeclarationContext;
+import desi.DesiGrammarParser.FactorContext;
 import desi.DesiGrammarParser.ProgramContext;
+import desi.DesiGrammarParser.TermContext;
 
 public class DesiCompiler extends DesiGrammarBaseVisitor{
 
@@ -11,5 +15,28 @@ public class DesiCompiler extends DesiGrammarBaseVisitor{
 	public Object visitProgram(ProgramContext ctx) {
 		return visitChildren(ctx); 
 	}
+	
+	
+	
+	@Override
+	public Object visitDeclaration(DeclarationContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitDeclaration(ctx);
+	}
+	
+	
+	@Override
+	public Object visitTerm(TermContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitTerm(ctx);
+	}
+	
+	
+	@Override
+	public Object visitFactor(FactorContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitFactor(ctx);
+	}
+	
 	
 }
