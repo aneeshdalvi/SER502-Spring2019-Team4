2f6d38d48eeb94fa325e354c78dbaff0a9c5baac
jspatel5 <jspatel5@asu.edu>
2019-04-23 21:37:29 -0700

    adding load instruction in runtime

--- src/runtime/DesiRuntime.java
+++ src/runtime/DesiRuntime.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Stack;
 
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
 public class DesiRuntime implements DesiRuntimeConstants{
 	
 	private Stack<HashMap<String, DataValues>> memoryStack = new Stack<>();
@@ -37,21 +39,78 @@ public class DesiRuntime implements DesiRuntimeConstants{
 	    private int executeInstructionHandler(String currentInstruction, int programCounter) {
 	    	
 	    String[] instructions = splitInstruction(currentInstruction);
-		System.out.println("instruction " + instructions[0]);
+		String instructionType = instructions[0];
+		
+	    switch(instructionType) {
+        case STORE_INSTRUCTION:
+            executeStoreInstruction(instructions);
+            break;
+	    }
+	    
 		return programCounter;
 	}
 
 
 
 	    private void initializeStackMemory() {
-
-
 	    	memoryStack.push(new HashMap<>());
-
 	    }
 	    
 	    private String[] splitInstruction(String instruction) {
 	        return instruction.split("\\s");
 	    }
+	    
+	    private void executeStoreInstruction(String[] instruction) {
+	        if(instruction[1].equals(ACCUMULATOR_REGISTER)) {
+	            setValue(ACCUMULATOR_REGISTER, getWildCardValue(instruction[2]));
+	        }
+	        else {
+	            setValue(instruction[1], getValue(instruction[2]));
+	        }
+	    }
+	    
+	    private void setValue(String identifier, DataValues value) {
+	        HashMap<String, DataValues> hashMap = memoryStack.peek();
+	        hashMap.put(identifier, value);
+	        
+	    }
+	    
+	    private boolean isInt(String value) {
+	        try {
+	            Integer.parseInt(value);
+	            return true;
+	        } catch (NumberFormatException e) {
+	            return false;
+	        }
+	    }
+	    
+	    private boolean isBoolean(String value) {
+	        try {
+	        	Boolean.parseBoolean(value);
+	            return true;
+	        } catch (NumberFormatException e) {
+	            return false;
+	        }
+	    }
 
+	    private DataValues getValue(String identifier) {
+	        HashMap<String, DataValues> hashMap = memoryStack.peek();
+	        return hashMap.get(identifier);
+	    }
+
+	    
+	    private DataValues getWildCardValue(String value) {
+	        if(value.equals("NULL")) {
+	            return null;
+	        }
+	        else if(isInt(value)) {
+	            return new DataValues(Integer.parseInt(value));
+	        }
+	        else if(isBoolean(value)) {
+	            return new DataValues(Boolean.parseBoolean(value));
+	        }
+	        else {
+	            return getValue(value);
+	        }
+	    }
 }
