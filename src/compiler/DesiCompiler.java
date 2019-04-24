906f801080ceda5e95c876233a18e19795ac27bc
virajtalaty <virajtalaty@gmail.com>
2019-04-23 21:54:56 -0700

    Assignment operation

--- src/compiler/DesiCompiler.java
+++ src/compiler/DesiCompiler.java
@@ -6,6 +6,7 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 import desi.DesiGrammarParser.AssignmentIntegerContext;
 import desi.DesiGrammarParser.BlockContext;
 import desi.DesiGrammarParser.CommandContext;
+import desi.DesiGrammarParser.ExpressionNumberOnlyContext;
 import desi.DesiGrammarParser.ProgramContext;
 import desi.DesiGrammarBaseVisitor;
 
@@ -13,79 +14,53 @@ public class DesiCompiler extends DesiGrammarBaseVisitor{
 
 	
 	@Override
-		public Object visitProgram(ProgramContext ctx) {
-		
-			System.out.println("program");
-			return visitChildren(ctx);
-			
-			
-			/*System.out.println("Program Shuru");
-			System.out.println(ctx.getChildCount());
-			
-			System.out.println(ctx.getChild(0));
-			
-			System.out.println(ctx.getChild(1).getChild(0).getChild(0).getChild(0));
-			System.out.println(ctx.getChild(1).getChild(0).getChild(0).getChild(1));
-			System.out.println(ctx.getChild(1).getChild(0).getChild(0).getChild(2));
-			
-			System.out.println(ctx.getChild(1).getChild(0).getChild(0).getChild(3));
-			System.out.println(ctx.getChild(1).getChild(0).getChild(0).getChild(3).getChild(0));
-			
-			
-			System.out.println(ctx.getChild(2));
-			System.out.println("Program Khatam");*/
-		}
-	
-		@Override
-		public Object visitBlock(BlockContext ctx) {
+	public Object visitProgram(ProgramContext ctx) {
+		return visitChildren(ctx);
+	}
 
-			return visitChildren(ctx);
-		}
-	
-		@Override
-		public Object visitCommand(CommandContext ctx) {
-			// TODO Auto-generated method stub
-			
-//			System.out.println(ctx.getChildCount());
-//			System.out.println(ctx.getChild(0).getChild(0));
-			System.out.println("out command");
-			
-			return visitChildren(ctx);
-		}
-	
-		
-		@Override
-		public Object visitAssignmentInteger(AssignmentIntegerContext ctx) {
-			// TODO Auto-generated method stub
-			System.out.println("in assign");
-			System.out.println(ctx.getChildCount());
-			System.out.println(ctx.getChild(0));
-			System.out.println(ctx.getChild(1));
-			System.out.println(ctx.getChild(2));
-			System.out.println(ctx.getChild(3));
-			System.out.println("out assign");
-			return super.visitAssignmentInteger(ctx);
-		}
-		
-	/*
-@Override
-public Object visitAddition(AdditionContext ctx) {
-	System.out.println("in here");
-	visitChildren(ctx);
-	if(ctx.getChildCount() == 1)
-	{
-		System.out.println("in 1st if");
-		System.out.println(ctx.getChild(0));
+	@Override
+	public Object visitBlock(BlockContext ctx) {
+
+		return visitChildren(ctx);
 	}
-	else
-	{
-		System.out.println(ctx.getChild(2));
-		System.out.println("addition");
+
+	@Override
+	public Object visitCommand(CommandContext ctx) {
+		//System.out.println("command");
+		return visitChildren(ctx);
+	}
+
+	
+	@Override
+	public Object visitAssignmentInteger(AssignmentIntegerContext ctx) {
+		
+		if(ctx.getChildCount()==4)
+		{
+			visitChildren(ctx);
+			System.out.println("LOAD "  + ctx.getChild(1) + " ACC");
+		}
+		else if(ctx.getChildCount()==3)
+		{
+			System.out.println("in 3");
+			
+			visitChildren(ctx);
+			System.out.println("LOAD A ACC");
+			
+			System.out.println("temp " + ctx.getChild(0));
+			System.out.println("temp " + ctx.getChild(1));
+			System.out.println("temp " + ctx.getChild(2));
+		}
+		return null;
+		
+		
 	}
 	
-	return null;
-}	*/
-	
-	
+	@Override
+	public Object visitExpressionNumberOnly(ExpressionNumberOnlyContext ctx) {
+		// TODO Auto-generated method stub
+		System.out.println("child count" + ctx.getChildCount() );
+		System.out.println("LOAD ACC " + ctx.getChild(0));
+		return super.visitExpressionNumberOnly(ctx);
+	}
 }
 
