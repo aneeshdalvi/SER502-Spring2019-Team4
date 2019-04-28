765876c816cd9470f1c6bf669c92c3d423b37be2
Janice Abraham <janicejabraham@gmail.com>
2019-04-28 16:20:23 -0700

    Storing intermediate code as a file

--- src/compiler/CompilerMain.java
+++ src/compiler/CompilerMain.java
@@ -5,6 +5,9 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.gui.Trees;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.ANTLRInputStream;
 
@@ -15,19 +18,36 @@ import desi.DesiGrammarParser;
 public class CompilerMain {
 
 	public static void main(String[] args) throws IOException {
+
+		//Output file config
+		String outputPath = "./src/data/";
+		String outputFileExtension = ".vdesi";
+
+		//Input file config
+		String inputPath = "data/";
+		String inputFileName = "test";
+		String inputFileExtension = ".desi";
 		
-		ANTLRInputStream input = new ANTLRFileStream("data/test.desi");
+		ANTLRInputStream input = new ANTLRFileStream(inputPath + inputFileName + inputFileExtension);
+		
 		
 		DesiGrammarLexer lexer = new DesiGrammarLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		DesiGrammarParser parser = new DesiGrammarParser(tokens);
-		
 		ParseTree tree  = parser.program();
+		
 		DesiCompiler d = new DesiCompiler();
 		d.visit(tree);
-		Trees.inspect(tree, parser);
-		System.out.println(d.getOutput());
+		List<String> intermediateCode = Arrays.asList(d.getOutput().split("\\n"));
+		if(intermediateCode.size()>1)
+		{
+			PrintWriter writer = new PrintWriter(outputPath + inputFileName + outputFileExtension, "UTF-8");
+			for(int i=0; i<intermediateCode.size();i++)
+			{
+				writer.println(intermediateCode.get(i));
+			}
+			writer.close();
+		}
 		
-		//PrintWriter writer = new PrintWriter(path.replace("race", "intermediate"), "UTF-8");
 	}
 }
