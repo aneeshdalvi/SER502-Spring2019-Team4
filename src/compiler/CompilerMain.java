e076e4a1d21b5f9fe0f27f95186647ea630b3cbf
virajtalaty <virajtalaty@gmail.com>
2019-04-30 18:14:49 -0700

    Input files

--- src/compiler/CompilerMain.java
+++ src/compiler/CompilerMain.java
@@ -15,30 +15,12 @@ public class CompilerMain {
 
 	public static void main(String[] args) throws IOException {
 
-		int lastOccurance = 0;
-		
-		String file = args[0];
-		//String file = "data\\test.desi";
-		
-		lastOccurance = file.lastIndexOf('\\');
-		String inputPath = file.substring(0,lastOccurance+1);
-		String fileName = file.substring(lastOccurance+1);
-		
-		String inputFileName = fileName.split("\\.")[0];
-		String inputFileExtension = "." + fileName.split("\\.")[1];
-		
-		//Output file config
-		String outputPath = inputPath;
-		String outputFileExtension = ".vdesi";
-
-		String path = inputPath + inputFileName+inputFileExtension;
-		
-		if (args.length > 0) {
+        String path = "data\\test.desi";
+        if (args.length > 0) {
             path = args[0];
         }
 
-		CharStream code = CharStreams.fromFileName(path);
-		
+        CharStream code = CharStreams.fromFileName(path);
 		DesiGrammarLexer lexer = new DesiGrammarLexer(code);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		DesiGrammarParser parser = new DesiGrammarParser(tokens);
@@ -49,7 +31,7 @@ public class CompilerMain {
 		List<String> intermediateCode = Arrays.asList(d.getOutput().split("\\n"));
 		if(intermediateCode.size()>1)
 		{
-			PrintWriter writer = new PrintWriter(outputPath + inputFileName + outputFileExtension, "UTF-8");
+	        PrintWriter writer = new PrintWriter(path.replace("desi", "vdesi"), "UTF-8");
 			for(int i=0; i<intermediateCode.size();i++)
 			{
 				writer.println(intermediateCode.get(i));
