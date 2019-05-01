25613825f9160be033e1607bf7ebc53d04c68fa1
virajtalaty <virajtalaty@gmail.com>
2019-04-30 21:15:26 -0700

    Adding build file

--- src/runtime/DesiRuntimeMain.java
+++ src/runtime/DesiRuntimeMain.java
@@ -9,35 +9,27 @@ import org.antlr.v4.runtime.CharStreams;
 public class DesiRuntimeMain {
 	
 	public static void main(String[] args) throws IOException {
-		
-		 String inputFile = "inputTestFile1.vdesi";
-
-	        String inputPath = "data/";
-
-	        String path = "data\\inputTestFile5.vdesi" ;
-	        //System.out.println(path);
 
 	        if (args.length > 0) {
-	            path = args[0];
+	        	String path = args[0];
+	        	
+	        	CharStream code = CharStreams.fromFileName(path);
+
+		        PrintWriter writer = new PrintWriter(path.replace("vdesi", "output"), "UTF-8");
+
+		        try {
+
+		        	DesiRuntime runtime = new DesiRuntime(code.toString().replaceAll("\r", ""));
+		            runtime.execute();
+		            System.out.println("Output of the program \n"+runtime.getOutputData());
+		            writer.println("Output of the program ");
+		            writer.println(runtime.getOutputData());
+		        }
+		        catch(Exception e) {
+		        	e.printStackTrace();
+		        }
+		        writer.close();
 	        }
-
-	        CharStream code = CharStreams.fromFileName(path);
-
-	        PrintWriter writer = new PrintWriter(path.replace("vdesi", "output"), "UTF-8");
-
-	        try {
-
-	        	DesiRuntime runtime = new DesiRuntime(code.toString().replaceAll("\r", ""));
-	            runtime.execute();
-	            System.out.println("Output of the program \n"+runtime.getOutputData());
-	            writer.println("Output of the program ");
-	            writer.println(runtime.getOutputData());
-	        }
-	        catch(Exception e) {
-	        	e.printStackTrace();
-	        }
-	        writer.close();
-		
 	}
 
 }
